319bcac48edbfec4305dd768432c99232c6e1163
Keiran <keiran@carpela.com>
2013-03-11 17:38:36 +0000

    rails_apps_composer: initial commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Jkwedding::Application.config.secret_token = '30ad6e07054d738c121c64f3b12687c6adbd3e5a013a451de320545b3df563e24ffacfc4c9e39efd9940a04e50f8d39fc4d690d46ecf702823c87d5aa991c46e'
+Jkwedding::Application.config.secret_token = 'ae2f6a9967c034fee739dedd722d5873711b35e7a524f4be37395e9658d3b85bc06323d161aa65adbc88b179649664038d20c3e79332b1ccdebf02860925dcfe'
